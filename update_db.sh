944a02932dc2670434076cd6662fafb13bb1c3ea
sarahoakman <41679973+sarahoakman@users.noreply.github.com>
2020-03-24 01:18:18 +1100

    update who.db

--- update_db.sh
+++ update_db.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-heroku ps:stop run -a teletubbies-who-api 
-python updatebot.py 
+python updatebot.py
 git add . 
 git commit -m "update who.db" 
 git push 
-heroku dyno:restart -a teletubbies-who-api 
